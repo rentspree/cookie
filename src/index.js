0245afe843b4a8af0b1d303400596453f3321afe
Jirat Boondam <jirat@nf-solution.com>
2018-07-05 15:37:26 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,109 @@
+import Cookies from "universal-cookie"
+import _ from "lodash"
+
+let options = {}
+const cookies = new Cookies()
+
+/**
+ * Set options for the next cookies that will be stored.
+ *
+ * @export
+ * @param {Object} [options={}]
+ */
+export function setOption(op = {}) {
+  options = _.merge(options, op)
+}
+
+/**
+ *  Add a listener to when a cookie is set or removed.
+ *
+ * @export
+ * @param {function} callback
+ */
+export function addChangeListener(callback) {
+  cookies.addChangeListener(callback)
+}
+
+/**
+ * Remove a listener from the change callback.
+ *
+ * @export
+ * @param {function} callback
+ */
+export function removeChangeListener(callback) {
+  cookies.removeChangeListener(callback)
+}
+
+/**
+ * Set a cookie value.
+ *
+ * @export
+ * @param {string} localStorageName Cookie's name
+ * @param {string, Object} data Cookie's value
+ */
+export function setLocalItem(localStorageName, data) {
+  // SAVE TO COOKIE
+  cookies.set(localStorageName, data, options)
+}
+
+/**
+ * Get a cookie value.
+ *
+ * @export
+ * @param {string} localStorageName Cookie's name
+ * @param {Object} [options={}] Cookie's options
+ * @returns Return cookie or false if cookie is not found.
+ */
+export function getLocalItem(localStorageName, op = {}) {
+  // GET ITEM FROM LOCAL STORAGE
+  const cookie = cookies.get(localStorageName, op)
+  if (cookie) {
+    return cookie
+  } else {
+    return false
+  }
+}
+
+/**
+ * Get all cookies.
+ *
+ * @export
+ * @param {Object} [options={}]
+ * @returns Return all cookies.
+ */
+export function getAllLocalItem(op = {}) {
+  return cookies.getAll(op)
+}
+
+/**
+ * Remove a cookie.
+ *
+ * @export
+ * @param {string} localStorageName Cookie's name
+ * @param {Object} [options={}] Cookie's options
+ */
+export function removeLocalItem(localStorageName, op = {}) {
+  // REMOVE ITEM FROM COOKIE
+  const sentOptions = _.merge({
+      path: "/"
+    },
+    op
+  )
+  cookies.remove(localStorageName, sentOptions)
+}
+
+/**
+ * Remove all cookies.
+ *
+ * @export
+ * @param {string} prefix Prefix of the cookie's name to remove
+ */
+export function removeAllLocalItem(prefix) {
+  const storageName = cookies.getAll()
+  Object.keys(storageName).forEach(function (key) {
+    const regex = new RegExp("^" + prefix + ".*", 'i')
+    if (regex.test(key)) {
+      removeLocalItem(key)
+    }
+  })
+}
---
+++ test/index.test.js
@@ -0,0 +1,108 @@
+import Cookies from 'universal-cookie'
+import _ from 'lodash'
+import chai from 'chai'
+import sinon from 'sinon'
+import * as CookieUtils from '../src/index'
+
+var should = chai.should()
+
+describe("Cookie Utility", () => {
+  let listeners = []
+
+  afterEach(() => {
+    listeners.map((listener) => {
+      CookieUtils.removeChangeListener(listener)
+    })
+    if (listeners.length > 0) {
+      listeners.splice(0, listeners.length)
+    }
+  })
+
+  context("setOption", () => {
+    it("should be able to set options of cookies", () => {
+      const option = {
+        path: "/something"
+      }
+
+      const mergeStub = sinon.stub(_, "merge").returns({})
+
+      CookieUtils.setOption(option)
+      mergeStub.getCall(0).args[1].should.equal(option)
+      mergeStub.restore()
+    })
+  })
+
+  context("setLocalItem", () => {
+    it("should be able to save cookie", () => {
+      const cookieName = "woahTestSet"
+      const cookieValue = "test-set-cookie"
+
+      const listener = function ({
+        name,
+        value,
+        options
+      }) {
+        name.should.equal(cookieName)
+        value.should.equal(cookieValue)
+      }
+
+      CookieUtils.addChangeListener(listener)
+      listeners.push(listener)
+      CookieUtils.setLocalItem(cookieName, cookieValue)
+    })
+  })
+
+  context("getLocalItem", () => {
+    it("should be able to get cookie", () => {
+      const cookieName = "woahTestGet"
+      const cookieValue = "test-get-cookie"
+
+      CookieUtils.setLocalItem(cookieName, cookieValue)
+      const getCookie = CookieUtils.getLocalItem(cookieName)
+      getCookie.should.equal("test-get-cookie")
+    })
+  })
+
+  context("removeLocalItem", () => {
+    it("should be able to remove cookie", () => {
+      const cookieName = "woahTestRemove"
+      const cookieValue = "test-remove-cookie"
+
+      CookieUtils.setLocalItem(cookieName, cookieValue)
+      CookieUtils.removeLocalItem(cookieName)
+      const removeCookie = CookieUtils.getLocalItem(cookieName)
+      removeCookie.should.be.false
+    })
+  })
+
+  context("getAllLocalItem", () => {
+    it("should be able to get all cookies", () => {
+      const cookieName = "woahTestAllCookie"
+      const cookieValue = "test-all-cookies"
+      const amount = 4;
+      CookieUtils.removeAllLocalItem("woah")
+      for (let i = 1; i <= amount; i += 1) {
+        CookieUtils.setLocalItem(`${cookieName}${i}`, cookieValue)
+      }
+
+      const allCookies = CookieUtils.getAllLocalItem()
+      for (let i = 1; i <= amount; i += 1) {
+        allCookies.hasOwnProperty(`${cookieName}${i}`).should.be.true
+      }
+    })
+  })
+
+  context("removeAllLocalItem", () => {
+    it("should be able to remove all cookies that have \"woah\" at the start of their name", () => {
+      const cookieName = "noWoahTestGet"
+      const cookieValue = "test-get-cookie"
+      CookieUtils.setLocalItem(cookieName, cookieValue)
+
+      CookieUtils.removeAllLocalItem("woah")
+      const allCookies = CookieUtils.getAllLocalItem()
+
+      Object.entries(allCookies).length.should.equal(1)
+      allCookies.hasOwnProperty(cookieName).should.be.true
+    })
+  })
+})
